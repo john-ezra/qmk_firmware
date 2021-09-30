9579c440e8b35301d41e1f80c33649ca307a57f0
John Ezra <johnezra@dempseytech.com>
2021-09-29 18:00:09 -0700

    nanoBoot

--- keyboards/splitkb/kyria/keymaps/john-ezra/rules.mk
+++ keyboards/splitkb/kyria/keymaps/john-ezra/rules.mk
@@ -9,4 +9,11 @@ MOUSEKEY_ENABLE =  no      # Disable Mousekeys
 WPM_ENABLE = yes           # Enable WPM Counter (Works with default wpm files, but works better with vectorstorm updated wpm.c and wpm.h -> https://github.com/vectorstorm/qmk_firmware/tree/wpm_exact)
 KEY_OVERRIDE_ENABLE = yes  # Enable Key Overrides
 
+BOOTLOADER = hid           # This Keymap Uses nanoBoot
+BOOTLOADER_SIZE = 512
+PROGRAM_CMD = 	$(HID_BOOTLOADER_CLI) -mmcu=$(MCU) -w -v $(BUILD_DIR)/$(TARGET).hex
+
 SRC += oled.c
+
+# Use To ISP Flash: avrdude -p atmega32u4 -c avrisp -U flash:w:/Users/DempseyTech/nanoBoot/nanoBoot.hex:i -P /dev/cu.usbmodem123451 -U lfuse:w:0x7F:m -U hfuse:w:0xD6:m -U efuse:w:0xC7:m
+# Use To Flash: qmk flash -kb splitkb/kyria/rev1 -km john-ezra -bl hid_bootloader
